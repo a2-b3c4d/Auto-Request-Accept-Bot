9d56e7218b0d0d71e042ea6cf15f4faf85d08b31
Tharunfitfun <tharunfitfun204@gmail.com>
2024-12-21 19:12:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, 
 pr0fess0r_99=Client(
     "𝗕𝗼𝘁 𝗦𝘁𝗮𝗿𝘁𝗲𝗱 𝗣𝗹𝗲𝗮𝘀𝗲 𝗦𝘂𝗯𝘀𝗰𝗿𝗶𝗯𝗲 𝗢𝗽𝘂𝘀𝗧𝗲𝗰𝗵𝘇",
     bot_token = os.environ["BOT_TOKEN"],
-    api_id = int(os.environ["26069929"]),
-    api_hash = os.environ["b0551dd4dd9e81b47fe6aa92173aff24"]
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
 )
 
 CHAT_ID=int(os.environ.get("CHAT_ID", None))
